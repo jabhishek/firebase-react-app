d848431e6f4dad1a3f6453c082a9e231e08c9753
Abhishek Jain <abhishek@toptal.com>
2017-07-05 21:57:20 +0100

    add react-router and redux

--- src/components/render-app.js
+++ src/components/render-app.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+
 import App from './App/App';
 import SignIn from './sign-in/Sign-in';
 import {
-  BrowserRouter as Router,
   Route
 } from 'react-router-dom';
 
-export const renderApp = () => {
+export const renderApp = (store, history) => {
   ReactDOM.render(
-    <Router>
-      <div>
-        <Route exact path="/" component={App}/>
-        <Route path="/signin" component={SignIn}/>
-        <Route path="/app" component={App}/>
-      </div>
-    </Router>,
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div>
+          <Route exact path="/" component={App}/>
+          <Route path="/signin" component={SignIn}/>
+          <Route path="/app" component={App}/>
+        </div>
+      </ConnectedRouter>
+    </Provider>,
     document.getElementById('root'));
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import reducers from './reducers/index';
+import createHistory from 'history/createBrowserHistory';
+
 import {renderApp} from './components/render-app';
 
 import './index.css';
 
-renderApp();
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+
+const history = createHistory();
+const reducer = combineReducers({...reducers,
+  router: routerReducer });
+
+const middleware = applyMiddleware(
+  // logger,
+  routerMiddleware(history)
+);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(middleware));
+
+renderApp(store, history);
