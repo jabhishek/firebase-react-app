b6013f98edd00a51d15088b5dea2fb581774a7c9
Abhishek Jain <abhishek@toptal.com>
2017-07-19 21:57:44 +0100

    add header with signin button

--- src/components/common/page-header/page-header.js
+++ src/components/common/page-header/page-header.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import {auth, googleAuthProvider} from '../../../firebase';
 import './page-header.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import {connect} from 'react-redux';
 
-export const PageHeader = () => {
+const signIn = () => {
+  auth.signInWithRedirect(googleAuthProvider)
+    .then(result => {
+      console.log('signed in', result);
+    });
+};
+
+const signOut = () => {
+  auth.signOut();
+};
+
+const RightComponent = ({user}) => {
+  if (user) {
+    return <RaisedButton label="Sign Out" primary={true} onClick={signOut} />;;
+  } else {
+    return <RaisedButton label="Sign In" primary={true} onClick={signIn} />;
+  }
+};
+
+export const PageHeader = ({user}) => {
   return (
     <div className="page-header">
-
+      <div className="page-header__left"></div>
+      <div className="page-header__right">
+        <RightComponent user={user}/>
+      </div>
     </div>
   );
 };
+const mapStateToProps = ({user}) => ({ user });
+export default connect(mapStateToProps)(PageHeader);
--- src/components/sign-in/Sign-in.js
+++ src/components/sign-in/Sign-in.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import {auth, googleAuthProvider} from '../../firebase';
 import './Sign-in.css';
-import RaisedButton from 'material-ui/RaisedButton';
-import {PageHeader} from '../common/page-header/page-header';
+import PageHeader from '../common/page-header/page-header';
 
 export default class Signin extends React.Component {
-  signin = () => {
-    auth.signInWithRedirect(googleAuthProvider)
-    .then(result => {
-      console.log('signed in', result);
-    });
-  }
-
-  signout = () => {
-    auth.signOut();
-  }
-
   render() {
     return (
       <div>
@@ -28,10 +15,6 @@ export default class Signin extends React.Component {
             </div>
           </div>
         </div>
-{/*
-        <RaisedButton label="Sign In" primary={true} onClick={this.signin} />
-        <RaisedButton label="Sign Out" primary={true} onClick={this.signout} style={ {marginLeft: 12} }/>
-*/}
       </div>
     );
   }
